cb6c645f4303a803b43dac69e70b6bd13a8c4394
MatiasRomeo <m.romeov@hotmail.com>
2023-07-16 00:18:12 -0400

    UserState

--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -12,7 +12,7 @@ export const Products = () => {
      title: "Producto 1",
      description: "Descripcion",
      price: 50444,
-     imageUrl: "https://d1ih8jugeo2m5m.cloudfront.net/2022/12/productos-mas-vendidos-del-mundo.jpg"
+     imageUrl: "https://www.lecuine.com/blog/wp-content/uploads/2014/06/cuchillo-santoku-global-lecuine.jpg"
    }, {
      id: "2",
      title: "Producto 1",
